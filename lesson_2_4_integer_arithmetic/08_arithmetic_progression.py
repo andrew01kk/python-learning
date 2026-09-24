df45e407eefb3ed5b449d71c4929d0fd414e990f
andrew01kk <andrewkyrych01@gmail.com>
2026-09-24 19:34:13 +0200

    day 4: lesson 2.4 — arithmetic progression

---
+++ lesson_2_4_integer_arithmetic/08_arithmetic_progression.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the first term: "))
+d = int(input("Enter the common difference: "))
+n = int(input("Enter the number of terms: "))
+print("The arithmetic progression is:", a + d * (n - 1))
